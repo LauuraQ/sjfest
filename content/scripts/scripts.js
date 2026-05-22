c470d4c4d5415d195939f9a0dbf54a45f99cdb0a
Yatvig <mephistonew@gmail.com>
2026-05-22 07:40:30 +0300

    Add dropdown logic

--- content/scripts/scripts.js
+++ content/scripts/scripts.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     // Переменные
     const header = document.querySelector('.header');
-
-
+    const menuDropDown = document.querySelector('.header__menu-item--submenu');
+    const menuDropDownOpen = document.querySelector('.header__menu-item--submenu--open');
 
 
     // Функции
+    //Отрисовка задника для хедера и смена цвета кнопок
     window.addEventListener('scroll', () => {
         if (window.scrollY > 0) {
             header.classList.add('header--scrolled');
@@ -14,4 +15,9 @@ document.addEventListener("DOMContentLoaded", () => {
             header.classList.remove('header--scrolled');
         }
     })
+    //Открытие-Закрытие выпадающего в меню навигации
+    menuDropDown.addEventListener('click', () => {
+        menuDropDown.classList.toggle('header__menu-item--submenu--open');
+    })
+
 });
